899bf46c313bef00741dbf2d1bede11dbc0008c9
xyzlast <xyzlast@github.com>
2013-11-06 23:22:28 +0900

    add new entities for cities add repositories for cities but some file are not implemented.

---
+++ domain/src/main/java/co/kr/daesung/app/center/domain/repo/cities/SejongRepository.java
@@ -0,0 +1,11 @@
+package co.kr.daesung.app.center.domain.repo.cities;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ykyoon
+ * Date: 11/5/13
+ * Time: 1:58 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface SejongRepository {
+}
