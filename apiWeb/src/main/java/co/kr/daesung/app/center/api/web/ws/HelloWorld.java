1c858efd24b1216f5d387f9a837ec2ecd583b129
xyzlast <xyzlast@github.com>
2013-12-03 15:02:27 +0900

    CXF configuration : wsdl support

---
+++ apiWeb/src/main/java/co/kr/daesung/app/center/api/web/ws/HelloWorld.java
@@ -0,0 +1,11 @@
+package co.kr.daesung.app.center.api.web.ws;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ykyoon
+ * Date: 12/2/13
+ * Time: 4:51 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface HelloWorld {
+}
